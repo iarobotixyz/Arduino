d3af48472b7f4e375587663f395a51144352830b
iarobotixyz <62524097+iarobotixyz@users.noreply.github.com>
2022-02-14 17:05:08 -0600

    Add files via upload

---
+++ ESP8266/Prototipo2/Prototipo_2_sonar_led.ino
@@ -0,0 +1,59 @@
+
+
+const int trigPin = 12;
+const int echoPin = 14;
+const int led = 2;
+
+
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_VELOCITY/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  if(distanceCm<=10)
+    {
+     digitalWrite(LED_BUILTIN, HIGH);   //
+     delay(50);                       // 
+      }
+  else
+    {
+     digitalWrite(LED_BUILTIN, LOW);    
+     delay(50);  
+      }
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("---------Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("----------");
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  
+  delay(200);
+}
