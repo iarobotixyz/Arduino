143d1c6a4855b9546d20232d5aea5fa68ac0a1b1
iarobotixyz <62524097+iarobotixyz@users.noreply.github.com>
2023-02-09 18:17:37 -0600

    Add files via upload

---
+++ ESP8266/ArduinoDroid/Ejemplo4_Blink_GPIO_Display7_Digitos3y4_vahs_Frecuencia.ino
@@ -0,0 +1,41 @@
+/*Blink_GPIO_ESP8266_DISPLAY3Y4*/
+int frecuencia=150;
+void setup() {
+  pinMode(16, OUTPUT);//D0//ṔRIMER ROJO == AZUL 1
+  pinMode(15, OUTPUT);//D8//BLANCO CS TXD2  //PUNTO
+  pinMode(14, OUTPUT);//D5//PRIMERO AMARILLO//D
+  pinMode(13, OUTPUT);//D7//ULTIMO AMARILLO //F
+  pinMode(12, OUTPUT);//D6//SEGUNDO AMARILLO//E
+  pinMode(10, OUTPUT);//SD3//VERDE SDD3    //G
+  pinMode(9, INPUT);//SD2//SDD2 SOLO INPUT //
+  pinMode(5, OUTPUT);//D1//SEGUNDO ROJO    //A
+  pinMode(4, OUTPUT);//D2//TERCER ROJO    // B
+  pinMode(3, OUTPUT);//RX//BLANCO RX      //
+  pinMode(2, OUTPUT);//D4//AMARILLO == AZUL 2
+  pinMode(1, OUTPUT);//TX//BLANCO TX     //
+  pinMode(0, OUTPUT);//D3//CUARTO ROJO   // C
+}
+  void once()
+{
+  digitalWrite(1, LOW); //TX
+  delay(frecuencia);
+  digitalWrite(3, LOW); //RX
+  delay(frecuencia);
+  digitalWrite(2, LOW); //AZUL
+  delay(frecuencia);
+  digitalWrite(16, LOW); //AZUL
+  delay(frecuencia);
+  }
+void loop() {
+  once();
+  delay(frecuencia*2);
+  digitalWrite(1, HIGH); //TX
+  digitalWrite(3, HIGH); //RX
+  digitalWrite(2, HIGH); //AZUL
+  digitalWrite(16, HIGH); //AZUL
+  digitalWrite(4, HIGH); //b
+  digitalWrite(0, HIGH); //c
+  digitalWrite(12, HIGH); //e
+  digitalWrite(13, HIGH); //f
+  delay(frecuencia);
+}
